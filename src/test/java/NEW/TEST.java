a4c70d1a2ad27a9ac157d4118f81451aacf97b6a
SU-35 <alexkarpov5792@gmail.com>
2022-04-11 23:17:43 +0300

    debug

--- src/test/java/NEW/TEST.java
+++ src/test/java/NEW/TEST.java
@@ -1,29 +1,29 @@
 package NEW;
 import io.restassured.response.Response;
-import jdk.jfr.StackTrace;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
-import static io.restassured.http.ContentType.JSON;
 
 
 public class TEST {
     @Test
-    public void check1(){
+    public void check1() {
         Response response = given()
                 .baseUri("https://petstore.swagger.io/v2")
                 .basePath("/pet/findByStatus")
                 .header("api_key", "api_key")
-                .queryParam("status", "available")
+                .queryParam("status", "pending")
                 .header("Accept", "application/json")
-                .header("Content-Type","application/json;;charset=UTF-8")
+                .header("Content-Type", "application/json;;charset=UTF-8")
                 .header("Accept-Tncoding", "gzip, deflate,br")
                 .when()
-                .get ()
+                .get()
                 .then()
                 .extract().response();
-        assertEquals(201, response.getStatusCode(), "Status code "+response.getStatusCode());
-    }
-}
+        assertEquals(200, response.getStatusCode(), "Status code " + response.getStatusCode());
+        System.out.println(response.statusCode());
+    }}
+
+
 
